9d8b8d8ba75906e481ed8f7a3c9ba066d1bf8a96
NehaTelhan <NehaTelhan@users.noreply.github.com>
2017-04-12 00:43:58 -0400

    Added a gaussian_pyr_five that returns the images as arrays of floats

--- gaussian_pyramid.py
+++ gaussian_pyramid.py
@@ -13,11 +13,10 @@ max_y_height = 10
 #
 filelist = ['AmazonDriveDownload/txt_136.jpg']
 
-
-''' 
-Shows each image in the typical plot scaled down until it can be scaled down no more (1x1)
-'''
 def gaussian_pyr_one():
+    '''
+    Shows each image in the typical plot scaled down until it can be scaled down no more (1x1)
+    '''
     for filename in glob.glob('AmazonDriveDownload/*.jpg'):
         image = skimage.io.imread(filename)
         # print(image.shape, "Look here")
@@ -42,8 +41,8 @@ def gaussian_pyr_one():
         # print("Done")
         plt.show()
 
-''' prints image as an array of values and appends to a super array'''
 def gaussian_pyr_two():
+    ''' prints image as an array of values and appends to a super array; also saves the image at it's new lower resolution '''
     super_arr = []
     #for filename in filelist:
     for filename in glob.glob('AmazonDriveDownload/*.jpg'):
@@ -64,13 +63,13 @@ def gaussian_pyr_two():
 
         lower_res = cv2.pyrDown(image,dstsize=(16, 16))
         super_arr.append(lower_res)
-        # print("Lower Resolution Array", lower_res)
-        # cv2.imwrite("Lower_res.jpg", lower_res)
+        print("Lower Resolution Array", lower_res)
+        cv2.imwrite("Lower_res.jpg", lower_res)
 
     print("Super Array", super_arr)
 
-''' prints numerial representation of image as array'''
 def gaussain_pyr_three():
+    ''' prints numerial representation of image as array'''
     image = 'AmazonDriveDownload/txt_136.jpg'
 
     G = image.copy()
@@ -81,16 +80,32 @@ def gaussain_pyr_three():
 
     print(gpImage)
 
-''' Prints the image as floats numbers'''
 def gaussain_pyr_four():
+    ''' Prints the image as floats numbers'''
+
     image = 'AmazonDriveDownload/txt_136.jpg'
     image = skimage.io.imread(image)
 
     rows, cols = image.shape
-    image = skimage.transform.resize(image, (16, 16))
+    image = skimage.transform.resize(image, (20, 10))
     plt.show(image)
     print (image)
 
+
+def gaussian_pyr_five():
+    super_arr = []
+    for filename in glob.glob('AmazonDriveDownload/*.jpg'):
+        image = skimage.io.imread(filename)
+        rows = image.shape[0]
+        cols = image.shape[1]
+        # print("Rows:", rows)
+        # print("Cols:", cols)
+        # print("Dim:", dim)
+        image_arr = skimage.transform.resize(image, (int((2/3)*rows), int((2/3)*cols)))
+        # image_arr = skimage.transform.resize(image, (20, 10))
+        # print(image_arr)
+        super_arr.append(image_arr)
+    print(super_arr)
 
 
 
@@ -98,4 +113,5 @@ if __name__ == "__main__":
     # gaussian_pyr_one()
     # gaussian_pyr_two()
     # gaussain_pyr_three()
-    gaussain_pyr_four()
+    # gaussain_pyr_four()
+    gaussian_pyr_five()
