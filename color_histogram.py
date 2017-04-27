cc795ad22bbf49bb81f44e04774bb0defb94ef5f
Neha Telhan <nt7ab@virginia.edu>
2017-04-26 21:24:37 -0400

    updating color histogram to all can see. still need to calculate the sum of squared differences max value but don't understand output of print.historgram.

--- color_histogram.py
+++ color_histogram.py
@@ -4,13 +4,11 @@ import skimage.io
 import glob
 import cv2
 
-
 def center_histogram(coordinate, rows, cols):
     '''
     Input is:
         - I NEED TO CALCULATE THE FOLLOWING BASED OFF THE ORIGINAL IMAGE: start coordinate, height of image, width of image
         - the "box" around the image as denoted by a start coordinate, height of that "box", and width of the "box"
-
         - So take in original picture, the coordinate of the start of the box on the text_box, the number of rows (height) of that textbox and the number of columns of that textbox (width)
     :return:
         Two color histograms?!
@@ -18,7 +16,8 @@ def center_histogram(coordinate, rows, cols):
 
     # for filename in glob.glob('NehaRandoImages/*.jpg'):
     #     image = skimage.io.imread(filename)
-    original_image_path = 'NehaRandoImages/file0001625591306.jpg'
+    original_image_path = 'NehaRandoImages/file0001625591306.jpg' #Super Autumn Colors
+    # original_image_path = 'NehaRandoImages/file0001735386118.jpg' #Super Blue Image
     original_image = cv2.imread(original_image_path)
     color = ('b', 'g', 'r')
     original_rows = original_image.shape[0]
@@ -32,27 +31,29 @@ def center_histogram(coordinate, rows, cols):
     #An array to hold the four center rows of the textbox
     center_four_rows = []
 
-    #Finds the center of the textbox
-    #and adds the two rows above and below the middle line to array
-    #from the left of the textbox to the right
+    '''Finds the center of the textbox
+    and adds the two rows above and below the middle line to array
+    from the left of the textbox to the right'''
     for i in range((y_pix+box_height//2)-2, (y_pix+box_height//2)+2):
         center_four_rows.append(original_image[i][x_pix:x_pix+box_width])
 
-    #Make the color histogram and show the colors (RGB) as lines
+    #Make the color histogram for the text box's center rows and show the colors (RGB) as lines
     for i, col in enumerate(color):
-        histr = cv2.calcHist(center_four_rows, [i], None, [256], [0, 256])
-        plt.plot(histr, color = col)
+        histrA = cv2.calcHist(center_four_rows, [i], None, [256], [0, 256])
+        # cv2.normalize(histrA, histrA, 0, 255, cv2.NORM_MINMAX) #do I even need this line?
+        plt.title('Histogram for Text Boxs 4 Center Rows')
+        plt.plot(histrA, color=col)
         plt.xlim([0,256])
     plt.show()
+    print("Histogram A: ", histrA)
 
-
-    #An array to hold the four outside rows
-    #two rows from the top of the outside of the textbox
-    #two rows from the bottom of the outside of the textbox
+    '''An array to hold the four outside rows
+    two rows from the top of the outside of the textbox
+    two rows from the bottom of the outside of the textbox'''
     outer_four_rows = []
 
-    #Loops through the two entire rows from edge to edge of picture
-    #two rows above...
+    '''Loops through the two entire rows from edge to edge of picture
+    two rows above...'''
     for i in range((y_pix - 2), y_pix):
         outer_four_rows.append(original_image[i])
 
@@ -60,13 +61,31 @@ def center_histogram(coordinate, rows, cols):
     for i in range((y_pix + box_height), y_pix+box_height+2):
         outer_four_rows.append(original_image[i])
 
-    #Make the color histogram and show the colors (BGR) as lines
+    #Make the color histogram for the whole images two upper + two lower rows show the colors (BGR) as lines
     for i, col in enumerate(color):
-        histr = cv2.calcHist(outer_four_rows, [i], None, [256], [0, 256])
-        plt.plot(histr, color=col)
+        histrB = cv2.calcHist(outer_four_rows, [i], None, [256], [0, 256])
+        # cv2.normalize(histrB, histrB, 0, 255, cv2.NORM_MINMAX)#do I even need this line?
+        plt.title('Histogram for Entire Images 2 Upper/Lower Rows')
+        plt.plot(histrB, color=col)
         plt.xlim([0, 256])
     plt.show()
+    print("Histogram B: ", histrB)
 
+    '''
+    max value difference between two histograms is the intensity of the greatest difference in color (intensity = avg od color)
+    (SSD)
+    '''
+    # print("HistrA", histrA, len(histrA))
+    # print("HistrB", histrB, len(histrB))
+    ssd = []
+    for a in histrA:
+        for b in histrB:
+            diff = a - b
+            # print("A", a, "B", b , "DIFF" , diff)
+            ssd.append(diff**2)
+    print("MAX", max(ssd))
+
+    print("Compare:", cv2.compareHist(histrA, histrB, 1))
 
     # print("original_rows", original_rows)
     # print("original cols", original_cols)
